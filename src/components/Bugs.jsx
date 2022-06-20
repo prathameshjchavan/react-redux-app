581be475f23c62dbef75e18a7ee105af32f5fe39
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-06-20 11:00:25 +0530

    fetch bugs from store

---
+++ src/components/Bugs.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+import StoreContext from "../contexts/storeContext";
+import { loadBugs } from "../store/bugs";
+
+class Bugs extends Component {
+	static contextType = StoreContext;
+
+	state = { bugs: [] };
+
+	componentDidMount() {
+		const store = this.context;
+
+		this.unsubscribe = store.subscribe(() => {
+			const bugsInStore = store.getState().entities.bugs.list;
+			if (this.state.bugs !== bugsInStore) this.setState({ bugs: bugsInStore });
+		});
+
+		store.dispatch(loadBugs());
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	render() {
+		return (
+			<ul>
+				{this.state.bugs.map(({ id, description }) => (
+					<li key={id}>{description}</li>
+				))}
+			</ul>
+		);
+	}
+}
+
+export default Bugs;
